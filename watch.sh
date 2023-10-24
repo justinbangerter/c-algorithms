2edb5cb1784bfc26a2c9d8859dcbb95b73293671
Justin Bangerter <jnbangerter@gmail.com>
2023-10-23 20:49:58 -0400

    clear the screen for automated tests

--- watch.sh
+++ watch.sh
@@ -1,1 +1,4 @@
-while inotifywait -e close_write,modify,move,delete -r src test; do ./compile-test.sh; done
+while inotifywait -e close_write,modify,move,delete -r src test; do
+    clear
+    ./compile-test.sh
+done
